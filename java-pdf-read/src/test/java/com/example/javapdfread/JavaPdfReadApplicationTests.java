535900125181b2671b072174435562ab084560d7
Raghavan alias Saravanan M <raghavan.muthu@outlook.com>
2023-02-13 08:13:44 +0530

    Source Updated

---
+++ java-pdf-read/src/test/java/com/example/javapdfread/JavaPdfReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javapdfread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPdfReadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
